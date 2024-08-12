4919f05509d31cd511ab8941c48eb980930582cd
Amanda <silva.amandinha225@gmail.com>
2024-08-11 22:28:45 -0300

    meu site

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var typed = new Typed (".text", {
+    strings:["Como introduzir ferramentas de segurança no meio pessoal e profissional?"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+var typed = new Typed (".h-four", {
+    strings:["Quais são os principais tipos de hackers e como eles diferem em seus objetivos e métodos?"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
